50b97b10ebd596660b31f1870a16a52e19299ecd
konstantinos1981 <konstantinos.georgiou1981@gmail.com>
2024-05-08 15:44:49 +0300

    Second commit: Cleaning up the code.

--- main.py
+++ main.py
@@ -3,91 +3,72 @@ import random
 
 
 def deal():
+    """deal function returns a random card from the deck"""
     cards_available = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card_index = random.randint(0,12)
-    dealed_card = cards_available[card_index]
-    return dealed_card
+    card_drawn = random.choice(cards_available)
+    return card_drawn
 
 
-def check_for_blackjack():
-    user_score = sum(user_cards)
-    comp_score = sum(computer_cards)
-    if user_score == 21:
-        if comp_score < 21:
-            print("You win. Blackjack!!")
-            return True
-        elif comp_score == 21:
-            print("Its a tie. Both you and the dealer have Blackjack")
-            return True
+def check_blackjack(hand):
+    """checks if a blackjack has been dealt"""
+    if sum(hand) == 21:
+        return 0
 
-    if comp_score == 21:
-        print("You loose... Dealer has Blackjack")
-        return True
 
-def check_for_winner(user , computer):
-
-    if sum(user) > 21:
-        return "You bust"
-
-    if sum(user) == sum(computer):
-        return "It is a tie"
-
-    if sum(user) > sum(computer):
-        return f"You win. Dealer has {sum(computer)}"
-
-    if sum(computer) > 21:
-        return f"Dealer busts with {sum(computer)}. You win"
-
+def check_scores(user, comp):
+    if sum(user) == sum(comp):
+        return "Its a tie"
+    if sum(comp) > 21:
+        return "Dealer busts"
     else:
-        return f"Dealer wins with {sum(computer)}"
+        return "Dealer wins"
 
 
-while True:
+def play_game():
     print(logo)
-    play = input(f"Start a new game of BlackJack? Type 'y' or 'n' ")
-    if play == 'y':
-        user_cards = []
-        computer_cards = []
+    '''initializing the user and computer cards'''
+    user_cards = []
+    computer_cards = []
+    '''Dealing the user and computer initial hands'''
+    for _ in range(2):
         user_cards.append(deal())
         computer_cards.append(deal())
-        user_cards.append(deal())
-        computer_cards.append(deal())
-        while True:
-            blackjack = check_for_blackjack()
-            if blackjack:
-                break
 
-            while sum(user_cards) < 21:
-                deal_card = input(f"Your score is {sum(user_cards)}. Dealer has {computer_cards[0]}. "
-                                  f" Do you wish to draw another one? y/n")
+    user = check_blackjack(user_cards)
+    comp = check_blackjack(computer_cards)
 
-                if deal_card == 'y':
-                    user_cards.append(deal())
-                    print(user_cards)
-                    for index, item in enumerate(user_cards):
-                        if item == 11:
-                            if sum(user_cards) > 21:
-                                user_cards[index] = 1
-                                print(user_cards)
-                                continue
+    if user == 0:
+        if comp == 0:
+            print("You both have a blackjack. Its a tie")
+            return
+        else:
+            print("You win with a Blackjack")
+            return
+    if comp == 0:
+        print("Dealer has a blackjack. You loose")
+        return
 
-                elif deal_card == 'n':
-                    break
-
-            while sum(computer_cards) < 17:
-                computer_cards.append(deal())
-
-
-
-            print(check_for_winner(user_cards, computer_cards))
+    while sum(user_cards) < 21:
+        draw_card = input(f"Your hand so far is {user_cards}: Your score is {sum(user_cards)} \n"
+                          f"Dealer has {computer_cards[0]} \n"
+                          f"Do you wish to draw another card? (y/n) ")
+        if draw_card == 'y':
+            user_cards.append(deal())
+        if sum(user_cards) > 21 and 11 in user_cards:
+            user_cards.remove(11)
+            user_cards.append(1)
+        if draw_card == 'n':
             break
 
-    elif play == 'n':
-        print("Thank you for playing...")
-        break
+    if sum(user_cards) > 21:
+        print("You bust")
+        return
 
-    else:
-        print("Incorrect selection. Try again")
-        continue
+    while sum(computer_cards) < 17:
+        computer_cards.append(deal())
+
+    print(check_scores(user_cards, computer_cards))
 
 
+while input("Start a new game of Blackjack? Type 'y'/ 'n' ") == 'y':
+    play_game()
